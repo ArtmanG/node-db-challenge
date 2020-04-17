4348f76ecbb3e60f5df08c1d05866faf6cec3f79
Gregory Hawman <g.hawman@gmail.com>
2020-04-17 10:10:02 -0700

    set up tables with foreign keys and the down statements

---
+++ data/migrations/20200417094639_projects.js
@@ -0,0 +1,45 @@
+exports.up = function(knex) {
+    return knex.schema
+        .createTable('projects', tbl => {
+            tbl.increments();
+            tbl.string('project_name').notNullable();
+            tbl.string('project_description');
+            tbl.boolean('project_completed').defaultTo(0);
+        })
+
+        .createTable('resources', tbl => {
+            tbl.increments();
+            tbl.string('resource_name').notNullable();
+            tbl.string('resource_description');
+            tbl.integer('project_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('projects')
+                .onUpdate('CASCADE')
+                .onDelete('CASCADE');
+
+        })
+
+        .createTable('tasks', tbl => {
+            tbl.increments();
+            tbl.string('task_description').notNullable();
+            tbl.string('task_notes');
+            tbl.boolean('task_complete').defaultTo(0);
+            tbl.integer('project_id')
+                .unsigned()
+                .notNullable()
+                .references('id')
+                .inTable('projects')
+                .onUpdate('CASCADE')
+                .onDelete('CASCADE');
+        });
+};
+
+exports.down = function(knex) {
+    return knex.schema
+        .dropTableIfExists('tasks')
+        .dropTableIfExists('resources')
+        .dropTableIfExists('projects');
+  
+};
